edd6aac95239f37ebe52f039bce74327486dd208
dawntale <fajarsetya@live.com>
2018-10-06 12:49:53 +0700

    Adding post and page template

---
+++ template-parts/content-template-fullwidth.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * Template Name: Full-Width Template
+ * Template Post Type: post, page
+ *
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _s
+ */
+
+?>

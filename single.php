9aea64a6ced03f98d486bb6f57210ef4c8d3e06e
dawntale <fajarsetya@live.com>
2018-10-08 18:33:31 +0700

    Typography and formating

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 <div class="row">
-	<div id="primary" class="content-area col-lg-8 pr-md-0">
+	<div id="primary" class="content-area col-lg-8">
 		<main id="main" class="site-main">
 
 		<?php

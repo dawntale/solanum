43dffec18af13332e37771ba5179dfc01257bafc
dawntale <fajarsetya@live.com>
2018-10-10 15:04:23 +0700

    Footer style

--- footer.php
+++ footer.php
@@ -13,19 +13,17 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container-fluid">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Powered by %s', '_s' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+	<footer id="colophon" class="site-footer">
+		<div class="site-info container-fluid">
+			<span class="site-credit">
+				&#169; <?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo bloginfo( 'name' ); ?></a> - All Right Reserved.
+			</span>
+			<span class="theme-credit">
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Dawntale</a>' );
+				printf( esc_html__( 'Theme %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">dawntale</a>' );
 				?>
+			</span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
